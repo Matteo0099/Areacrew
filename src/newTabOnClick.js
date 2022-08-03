9943e8c1ce80e17bf95afed9e49bc4507ee08051
Matty <matteomania05@gmail.com>
2022-08-03 11:16:05 +0200

    Restyling

---
+++ src/newTabOnClick.js
@@ -0,0 +1,7 @@
+function test(){
+    url = "http://127.0.0.1:5500/img/Logo/LogoAreaCrewVerro.jpeg";
+    img = '<img src="'+url+'">';
+    popup = window.open();
+    popup.document.write(img);                        
+    popup.print();
+}
